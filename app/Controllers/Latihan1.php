3f18445422ec7c4bd6484b4e82408b49e2eacefd
fariz1705 <farizthn@gmail.com>
2023-04-01 11:18:00 +0700

    mengerjakan contoh3

--- app/Controllers/Latihan1.php
+++ app/Controllers/Latihan1.php
@@ -16,4 +16,12 @@ class Latihan1 extends BaseController
         $hasil = $model->jumlah($n1, $n2);
         echo "Hasil Penjumlahan dari ". $n1 ." + ". $n2 ." = " .$hasil;
     }
+    public function penjumlahan_1($n1, $n2)
+    {
+        $model_latihan1 = new Model_latihan1();
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n2;
+        $data['hasil'] = $model_latihan1->jumlah($n1, $n2);
+        return view('view-latihan', $data);
+    }
 }
