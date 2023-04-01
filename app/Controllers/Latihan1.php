fc9ac340e9bc6a5ea890dc3a136857352fdf0951
fariz1705 <farizthn@gmail.com>
2023-04-01 10:53:06 +0700

    mengerjakan contoh1

---
+++ app/Controllers/Latihan1.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\Model_latihan1;
+
+class Latihan1 extends BaseController
+{
+    public function index()
+    {
+        echo "Selamat Datang Fariz, selamat belajar Web Programming";
+    }
+}
